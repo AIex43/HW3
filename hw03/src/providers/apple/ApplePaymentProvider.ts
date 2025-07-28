1c0e0160fef6a93eb88aa9281516d71248b7fe06
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-28 15:13:01 +0300

    Update ApplePaymentProvider.ts

--- hw03/src/providers/apple/ApplePaymentProvider.ts
+++ hw03/src/providers/apple/ApplePaymentProvider.ts
@@ -1,1 +1,17 @@
-// TODO: Implement the ApplePaymentProvider class
+import { PaymentProvider } from "../../core/PaymentProvider";
+
+export class ApplePaymentProvider implements PaymentProvider {
+  authorize(amount: number): string {
+    const txId = `ap_${Math.random().toString(36).substr(2, 6)}`;
+    console.log(`[ApplePay] Authorizing $${amount}`);
+    return txId;
+  }
+
+  capture(transactionId: string): void {
+    console.log(`[ApplePay] Capturing transaction ${transactionId}`);
+  }
+
+  refund(transactionId: string): void {
+    console.log(`[ApplePay] Refunding transaction ${transactionId}`);
+  }
+}
