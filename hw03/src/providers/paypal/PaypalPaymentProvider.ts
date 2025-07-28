2369b7e7e3bde863e51a4d55b4985180ea5d6954
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-28 15:14:40 +0300

    Update PaypalPaymentProvider.ts

--- hw03/src/providers/paypal/PaypalPaymentProvider.ts
+++ hw03/src/providers/paypal/PaypalPaymentProvider.ts
@@ -1,1 +1,17 @@
-// TODO: Implement the PaypalPaymentProvider class
+import { PaymentProvider } from "../../core/PaymentProvider";
+
+export class PaypalPaymentProvider implements PaymentProvider {
+  authorize(amount: number): string {
+    const txId = `pp_${Math.random().toString(36).substr(2, 6)}`;
+    console.log(`[PayPal] Authorizing $${amount}`);
+    return txId;
+  }
+
+  capture(transactionId: string): void {
+    console.log(`[PayPal] Capturing transaction ${transactionId}`);
+  }
+
+  refund(transactionId: string): void {
+    console.log(`[PayPal] Refunding transaction ${transactionId}`);
+  }
+}
