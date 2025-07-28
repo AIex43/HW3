ed4ffb0c7481a4781fe01c08eb704033a97fc7b9
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-28 15:15:21 +0300

    Update StripePaymentProvider.ts

--- hw03/src/providers/stripe/StripePaymentProvider.ts
+++ hw03/src/providers/stripe/StripePaymentProvider.ts
@@ -1,1 +1,17 @@
-// TODO: Implement the StripePaymentProvider class
+import { PaymentProvider } from "../../core/PaymentProvider";
+
+export class StripePaymentProvider implements PaymentProvider {
+  authorize(amount: number): string {
+    const txId = `tx_${Math.random().toString(36).substr(2, 6)}`;
+    console.log(`[Stripe] Authorizing $${amount}`);
+    return txId;
+  }
+
+  capture(transactionId: string): void {
+    console.log(`[Stripe] Capturing transaction ${transactionId}`);
+  }
+
+  refund(transactionId: string): void {
+    console.log(`[Stripe] Refunding transaction ${transactionId}`);
+  }
+}
